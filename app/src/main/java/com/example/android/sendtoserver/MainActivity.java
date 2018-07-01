1658023b4b8db8807c7e23b5e2fe19e133c65202
Srajan Gupta <srajaninnov@gmail.com>
2018-07-01 23:51:19 +0530

    Code Changes

--- app/src/main/java/com/example/android/sendtoserver/MainActivity.java
+++ app/src/main/java/com/example/android/sendtoserver/MainActivity.java
@@ -6,13 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,54 +24,54 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
     String Like;
-    String server_url = "http://127.0.0.1/update_info.py";
+    TextView l;
+    String server_url = "http://codersdiary-env.jrpma4ezhw.us-east-2.elasticbeanstalk.com/likes/";
     AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.bn);
-        builder = new AlertDialog.Builder(MainActivity.this);
+        button = (Button) findViewById(R.id.bn);
+        l = (TextView) findViewById(R.id.likes);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String like;
-                like = Like;
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                //this is the url where you want to send the request
+                //TODO: replace with your own url to send request, as I am using my own localhost for this tutorial
+                //String url = "http://192.168.1.7/AndroidVolley/androidVolley.php";
+
+                // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                builder.setTitle("Server Response");
-                                builder.setMessage("Response :"+response);
-                                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Like = "0";
-                                    }
-                                });
-                                AlertDialog alertDialog = builder.create();
-                                alertDialog.show();
+                                // Display the response string.
+                                l.setText(response);
                             }
-                        }
-                        , new Response.ErrorListener() {
+                        }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Error...",Toast.LENGTH_SHORT).show();
-                        error.printStackTrace();
+                        l.setText("That didn't work!");
                     }
-                }){
+                }) {
+                    //adding parameters to the request
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String,String> params = new HashMap<String, String>();
-                        params.put("1",like);
+                        Map<String, String> params = new HashMap<>();
+                        params.put("likes", "Great Job Mansi...");
+                        //params.put("email", _email.getText().toString());
                         return params;
-            }
+                    }
                 };
-
-                MySingleton.getInstance(MainActivity.this).addTorequestque(stringRequest);
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
-      }
+
     }
+}
+
 
